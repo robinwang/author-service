d341764da118f3e61c6d95f55e8f93efb4a2f524
Ubuntu <ubuntu@ip-172-31-3-9.us-east-2.compute.internal>
2017-10-10 23:18:41 +0000

    HW3

--- src/main/java/edu/cmu/mis/iccfb/controller/AuthorController.java
+++ src/main/java/edu/cmu/mis/iccfb/controller/AuthorController.java
@@ -1,6 +1,11 @@
 package edu.cmu.mis.iccfb.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +22,14 @@ import edu.cmu.mis.iccfb.service.AuthorService;
 public class AuthorController {    
     @Autowired
     private AuthorService authorService;    
+    @Autowired
+    private DiscoveryClient discoveryClient;
     
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+    		@PathVariable String applicationName) {
+    	return this.discoveryClient.getInstances(applicationName);
+    }
     @RequestMapping("/authorId")
     public Author getAuthorName(@RequestParam(value="authorId") long authorId) {
         Author a = authorService.findOne(authorId);
